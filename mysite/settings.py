298ca4ae4a62746f3a45cb96a935491e760becd5
trachyan11 <trachyan.11@gmail.com>
2019-10-29 19:26:23 +0200

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!d%fnfqkw94*=1v=fk&o(*pycq6n*x^%9xxn2szdu%(t6zangp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
